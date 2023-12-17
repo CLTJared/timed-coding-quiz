895ee611ffb675c444a4b1b08889b2ba357a0d94
Jared Elliott <bootcamp@jaredclt.com>
2023-12-17 17:04:37 -0500

    Additional element variables defined. Created setInterval for playing game.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,51 @@
-// Global Variables
+// Global Variables - Elements
+let btnStart = document.getElementById("gameStart");       //For starting the game
+let timerCount = document.getElementById("gameTime");      //For tracking remaining time left
+let gameBoard = document.getElementById("gameQuestion");   //For containing questions in game area
 
+// Global Variables - Other
+let gameOver = false;
+let gameTimer = 30;
 
-// Set Interval - Loop to Play Game within Timer
+// Game Over
+function gameEnd(gameStatus) {
+    //
+    switch(gameStatus) {
+        case true:
+            break;
+        case false:
+            break;
+        default:
+            break;
+    }
 
+}
 
+// Reset board/game conditions
+function resetGame() {
 
-// Event Listner Functions
+}
+
+// Play the game
+function playGame() {
+    btnStart.disabled = true;
+    // Game Timer
+    let playing = setInterval(function() {
+        gameTimer--;
+        console.log(gameTimer);
+        timerCount.textContent = gameTimer;
+
+        if (gameTimer >= 0) {
+            if (gameOver && gameTimer > 0) {
+                clearInterval(playing);
+            }
+        }
+        // Tests if time has run out
+        if (gameTimer === 0) {
+            btnStart.disabled = false;
+            clearInterval(playing);
+        }
+    }, 1000);
+}
+
+btnStart.addEventListener("click", playGame);
