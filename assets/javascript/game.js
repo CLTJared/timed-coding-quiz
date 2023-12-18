2d1accfdfe86b981481d6f58d0529795200a88cf
Jared Elliott <bootcamp@jaredclt.com>
2023-12-18 15:01:38 -0500

    Questions object and failsafe for timer going below 0.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,8 +2,11 @@
 let btnStart = document.getElementById("gameStart");    //For starting the game
 let btnQuestions = document.querySelectorAll("[data-type=\"question\"]");   //For starting the game
 let timerCount = document.getElementById("gameTime");   //For tracking remaining time left
-let gameBoard = document.getElementById("gameQuestion");    //For containing questions in game area
-let gameMsg = document.getElementById("gameMessage");   //For containing questions in game area
+let gameBoard = document.getElementById("gameArea");    //For containing questions in game area
+let gameStartMsg = document.getElementById("gameStartMsg"); 
+let gamePlayingMsg = document.getElementById("gamePlayBoard"); 
+let gameEndMsg = document.getElementById("gameEndMsg");
+let gameMsg = document.getElementById("gameMessage");   //For game status messages
 
 // Global Variables - Other
 let gameOver = false;
@@ -11,10 +14,48 @@ let gameStarted = false;
 let gameTimer = 60; //Default timer set to 60
 let btnQuestion;    //For looping through each question button and assigning an event listener
 
+// Create question(s) object
+let quizQuestions = [
+    //Question 1
+    {
+        prompt: 'Question 1',
+        options: ['a','b','c','d'],
+        answer: 'b',
+    },
+    //Question 2
+    {
+        prompt: 'Question 2',
+        options: ['a','b','c','d'],
+        answer: 'a',
+    },
+    //Question 3
+    {
+        prompt: 'Question 3',
+        options: ['a','b','c','d'],
+        answer: 'd',
+    },
+    //Question 4
+    {
+        prompt: 'Question 4',
+        options: ['a','b','c','d'],
+        answer: 'c',
+    },
+    //Question 5
+    {
+        prompt: 'Question 5',
+        options: ['a','b','c','d'],
+        answer: 'a',
+    }
+]
+
+
 // Reset board/game conditions
 function resetGame() {
-    gameTimer = 60;
-    gameMsg.setAttribute("style","background-color: transparant");
+    gameTimer = 10;
+    gameMsg.setAttribute("style","background-color: transparent");
+    gameStartMsg.setAttribute("style","display: none");
+    gamePlayingMsg.setAttribute("style","display: block");
+    gameEndMsg.setAttribute("style","display: none");
 }
 
 // Play the game
@@ -39,10 +80,11 @@ function playGame() {
             }
         }
         // Tests if time has run out
-        if (gameTimer === 0) { // Checks if time ran out for game
+        if (gameTimer <= 0 ) { // Checks if time ran out for game or somehow ran under 0
             btnStart.disabled = false;
-            gameMsg.textContent = 'Time ran out.';
-            gameMsg.setAttribute("background-color","var(--orange-warning)");
+            gamePlayingMsg.setAttribute("style","display: none");
+            gameEndMsg.setAttribute("style","display: block");
+            gameEndMsg.textContent = "Time Ran Out!";
             gameStarted = false;
             clearInterval(playing);
         }
@@ -58,11 +100,11 @@ function answerQuestion(event) {
         gameMsg.textContent = 'Answer Correct!';
         gameMsg.setAttribute("style","background-color: var(--orange-warning)");
     } else {
-        if(gameTimer>5){ gameTimer=gameTimer-5; } else { gameTimer=1; } //Incorrect test, remove 5 seconds
-        gameMsg.textContent = 'Answer Incorrect! Minus 5 Seconds.';
+        if(gameTimer>10){ gameTimer=gameTimer-10; } else { gameTimer=1; } //Incorrect test, remove 5 seconds
+        gameMsg.textContent = 'Answer Incorrect! Minus 10 Seconds.';
         gameMsg.setAttribute("style","background-color: var(--orange-warning)");
-    }
 
+    }
 }
 
 //Set Event Listener to the Start Game button
@@ -73,3 +115,8 @@ btnStart.addEventListener("click", playGame);
 btnQuestions.forEach(btnQuestion => {
     btnQuestion.addEventListener("click", answerQuestion);
 })
+
+
+//<button data-index="1" data-type="question">1. Answer 1</button>
+//When creating the loop for the questions container, these are some of the options.
+
